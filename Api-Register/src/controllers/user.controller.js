6147d354dc34674bb367a51b173aff828501fa03
KIy077 <thebinary0101@gmail.com>
2025-02-13 17:58:56 -0500

    Se termino de realizar la parte logica en controllers

--- Api-Register/src/controllers/user.controller.js
+++ Api-Register/src/controllers/user.controller.js
@@ -11,20 +11,23 @@ export const registerUser = async (req, res) => {
     const{email, password} = req.body;
 
     if (!email || !password){
-        return res.status(400).json({mensaje:"Datos incompletos"});
+        return res.status(400).json({messaje:"Datos incompletos"});
     }
 
     const hashedPassword = await bcrypt.hash(password,10)//encriptacion de la contraseña 
     users.push({email, password: hashedPassword});//guardando el usuario y la contraseña encriptada en la BD
-    res.status(201).json({mensaje: "Usuario registrado con exito"});
+    res.status(201).json({messaje: "Usuario registrado con exito"});
 }
 
-VOY A TOMAR AWITA
-ya vengo voy a buscar la comiADA
+export const loginuser = async (req, res) => {
+    const { email, password} = req.body;
+    
+    const user = users.find((u) => u.email === email);
+     
+     if (!user || !(await bcrypt.compare(password, user.password))){
+        return res.status(401).json({messaje: "El usuario o la contraseña son incorrecta"});
+    }
 
- BREAK DE 15 MINUTOS
-d
-e mi abuelo 
-nojodaBaaa
-siii recreooooo 
-JAJAJ
+    const token = jwt.sign({email: user.email}, process.env.JWT_SECRET, {expiresIn:"1h"})
+    res.json({token});
+}
